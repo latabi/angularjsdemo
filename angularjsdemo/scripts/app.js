5274a0e4a399c10206e4e228c2c73b6605e721ce
latab <latab@SDD-41>
2017-12-01 18:30:29 +0530

    Add project files.

---
+++ angularjsdemo/scripts/app.js
@@ -0,0 +1,25 @@
+﻿ var app=angular.module("PracticeApp", ["PracticeApp.controllers", "ngRoute"]);
+
+
+app.config(["$routeProvider", function($routeProvider) {
+    $routeProvider
+        .when("/",
+        {
+            templateUrl: "/Partials/Form.html",
+            controller: "MainController"
+        })
+        .when("/AddPlayer",
+        {
+            templateUrl: "/Partials/AddPlayer.html",
+            controller: "AddPlayerController"
+        })
+    .when("/Crud",
+        {
+            templateUrl: "/Partials/Crud.html",
+            controller: "CrudController"
+        })
+    .otherwise({ redirectTo: "/" });
+
+    
+}
+])
---
+++ angularjsdemo/scripts/controllers.js
@@ -0,0 +1,58 @@
+﻿angular.module("PracticeApp.controllers", []).
+controller("MainController",function($scope)
+{$scope.message="lata";
+})
+.controller("CrudController",function($scope)
+{
+    $scope.message = "crud";
+})
+    
+    
+    
+
+
+//.factory("PlayerService", ["$http", function ($http) {
+//    var fac = {};
+
+
+
+//    fac.GetPlayersfromDB = function () {
+//        return $http.get("/Player/GetPlayers")
+//    }
+
+
+//    fac.GetPlayerbyId = function (id) {
+//        return $http.get("/Player/GetPlayerbyId", {
+//            params: { id: id }
+//        });
+//    }
+
+
+//    fac.AddPlayer = function (player) {
+//        $http.post("/Player/AddPlayer", player).
+//        success(function (response) {
+//            alert(response.status);
+//        }
+//        )
+//    }
+
+
+//    fac.UpdatePlayer = function (player) {
+//        $http.post("/Player/UpdatePlayer", player)
+//        .success(function (response) {
+//            alert(response.status);
+//        }
+//    )
+//    }
+
+
+//    fac.DeletePlayer = function (id) {
+//        $http.post("/Player/DeletePlayer", { id: id })
+//        .success(function (response) {
+//            alert(response.status);
+//        }
+//        )
+//    }
+//    return fac;
+//}]
+//)
